d3d3b2eb2835b780da87050d0e338d7bd6df0f8c
N8theGr811 <104658019+N8theGr811@users.noreply.github.com>
2023-05-11 17:43:15 -0700

    Update task1.sql
    I added the preferences data, changed some of the table creation statements to fit the assignment, and debugged certain errors

--- task1.sql
+++ task1.sql
@@ -1,7 +1,4 @@
 -- Drop Tables
-
--- Testing
-
 -- in github
 DROP TABLE customer;
 DROP TABLE preferences;
@@ -16,23 +13,21 @@ CREATE TABLE customer (
     last_name VARCHAR2(20) NOT NULL,
     street VARCHAR2(50) NOT NULL,
     city VARCHAR2(20) NOT NULL,
-    state VARCHAR2(2) DEFAULT('CA') NOT NULL,
+    state VARCHAR2(2) DEFAULT 'CA' NOT NULL,
     zip NUMBER(5) NOT NULL,
     phone VARCHAR2(12) NOT NULL UNIQUE,
-    email VARCHAR2(50) NOT NULL UNIQUE,
-);
+    email VARCHAR2(50) NOT NULL UNIQUE);
 
 CREATE TABLE preferences (
     preference_id NUMBER(6) PRIMARY KEY,
     customer_id NUMBER(6) NOT NULL,
     make VARCHAR2(20) NOT NULL,
     model VARCHAR2(20) NOT NULL,
-    max_price NUMBER(12) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
-    CONSTRAINT preferences_end_date CHECK (end_date > start_date)
-);
+    max_price NUMBER(12),
+    start_date DATE DEFAULT SYSDATE NOT NULL,
+    end_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    CONSTRAINT preferences_end_date CHECK (end_date > start_date));
 
 
 
@@ -41,27 +36,51 @@ CREATE TABLE preferences (
 -- Insert Customer Data
 
 -- Inserts with DEFAULT clause
-INSERT INTO customer (first_name, last_name, street, city, zip, phone, email) 
-VALUES ('John', 'Smith', '1000 Los Valley Road', 'San Luis Obispo', '93408', '805.123.1234', 'jsmith@hotmail.com');
+INSERT INTO customer (customer_id, first_name, last_name, street, city, zip, phone, email) 
+VALUES (100, 'John', 'Smith', '1000 Los Valley Road', 'San Luis Obispo', '93408', '805.123.1234', 'jsmith@hotmail.com');
 INSERT INTO customer (first_name, last_name, street, city, zip, phone, email)
-VALUES ('Mary', 'Jones', '12 Peach Street', 'San Luis Obispo', '93401', '800.333.4444', 'mjones@hotmail.com');
+VALUES (101, 'Mary', 'Jones', '12 Peach Street', 'San Luis Obispo', '93401', '800.333.4444', 'mjones@hotmail.com');
 INSERT INTO customer (first_name, last_name, street, city, zip, phone, email) 
-VALUES ('Ian', 'Thomas', '268 Tunnel Ave', 'Los Angeles', '90004', '213.815.4351', 'ian.thomas@gmail.com');
+VALUES (102, 'Ian', 'Thomas', '268 Tunnel Ave', 'Los Angeles', '90004', '213.815.4351', 'ian.thomas@gmail.com');
 INSERT INTO customer (first_name, last_name, street, city, zip, phone, email) 
-VALUES ('Hank', 'Franklin', '321 Pine St', 'San Diego', '92101', '555-3456', 'alice.smith@example.com');
+VALUES (103, 'Hank', 'Franklin', '321 Pine St', 'San Diego', '92101', '555-3456', 'alice.smith@example.com');
 INSERT INTO customer (first_name, last_name, street, city, zip, phone, email) 
-VALUES ('Bill', 'Johnson', '654 Broadway', 'Fresno', '93721', '555-7890', 'mike.johnson@example.com');
+VALUES (104, 'Bill', 'Johnson', '654 Broadway', 'Fresno', '93721', '555-7890', 'mike.johnson@example.com');
 
 -- Inserts without DEFAULT clause
+INSERT INTO customer (customer_id, first_name, last_name, street, city, state, zip, phone, email) 
+VALUES (105, 'Sara', 'Lee', '111 Baker St', 'Los Angeles', 'CA', '90001', '555-5555', 'sara.lee@example.com');
 INSERT INTO customer (first_name, last_name, street, city, state, zip, phone, email) 
-VALUES ('Sara', 'Lee', '111 Baker St', 'Los Angeles', 'CA', '90001', '555-5555', 'sara.lee@example.com');
+VALUES (106, 'Tom', 'Jones', '222 Music Ave', 'San Francisco', 'CA', '94102', '555-6666', 'tom.jones@example.com');
 INSERT INTO customer (first_name, last_name, street, city, state, zip, phone, email) 
-VALUES ('Tom', 'Jones', '222 Music Ave', 'San Francisco', 'CA', '94102', '555-6666', 'tom.jones@example.com');
-INSERT INTO customer (first_name, last_name, street, city, state, zip, phone, email) 
-VALUES ('Karen', 'White', '333 Snow Dr', 'Sacramento', 'CA', '95814', '555-7777', 'karen.white@example.com');
+VALUES (107, 'Karen', 'White', '333 Snow Dr', 'Sacramento', 'CA', '95814', '555-7777', 'karen.white@example.com');
 INSERT INTO customer (first_name, last_name, steet, city, state, zip, phone, email) 
-VALUES ('Mark', 'Davis', '444 Lake Rd', 'San Diego', 'CA', '92101', '555-8888', 'mark.davis@example.com');
+VALUES (108, 'Mark', 'Davis', '444 Lake Rd', 'San Diego', 'CA', '92101', '555-8888', 'mark.davis@example.com');
 INSERT INTO customer (first_name, last_name, street, city, state, zip, phone, email) 
-VALUES ('Amy', 'Chen', '555 Hill Blvd', 'Fresno', 'CA', '93721', '555-9999', 'amy.chen@example.com');
+VALUES (109, 'Amy', 'Chen', '555 Hill Blvd', 'Fresno', 'CA', '93721', '555-9999', 'amy.chen@example.com');
 
 -- Insert Preferences Data
+-- One customer (102) has 3 preferences
+-- These make use of the default function for start date
+
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price)
+VALUES (1, 102, 'Pontiac', 'Firebird', 80000);
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price)
+VALUES (2, 102, 'Chevrolet', 'Corvette Coupe', 50000);
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price)
+VALUES (3, 102, 'Ferrari', '360 Modena', 150000);
+
+-- One customer (103) has 2 preferences
+-- Three customers (104, 105, 106) each have one preference
+-- These do not make use of the default function for start date
+
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price, start_date)
+VALUES (4, 103, 'Mazda', 'MX-5 Miata', 40000, '05/01/2001');
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price, start_date)
+VALUES (5, 103, 'Land Rover', 'Custom', 70000, '06/11/2002');
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price, start_date)
+VALUES (6, 104, 'Porsche', '914', 200000, '07/21/2003');
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price, start_date)
+VALUES (7, 105, 'Triumph', 'Spitfire', 90000, '08/22/2004');
+INSERT INTO preferences (preference_id, customer_id, make, model, max_price, start_date)
+VALUES (7, 106, 'Ford', 'Mustang', 60000, '09/11/2018');
