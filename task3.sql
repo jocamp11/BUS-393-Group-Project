34afb29cff1963acf66cd4f95ec010935c435f02
N8theGr811 <104658019+N8theGr811@users.noreply.github.com>
2023-05-23 15:30:11 -0700

    Update task3.sql

--- task3.sql
+++ task3.sql
@@ -1,1 +1,32 @@
+DROP TABLE Parts;
+DROP TABLE Services;
 
+CREATE TABLE Services (
+  Service_Code    VARCHAR2(20)    PRIMARY KEY,
+  Description     VARCHAR2(30)    NOT NULL,
+  Cost            NUMBER(6,2)     NOT NULL,
+  Price           NUMBER(6,2)     NOT NULL,
+  Months          NUMBER(3)       NOT NULL,
+  Mileage         NUMBER(7)       NOT NULL,
+  CHECK(Cost > 0),
+  CHECK(Price > 0),
+  CHECK(Months > 0),
+  CHECK(Mileage > 0));
+  
+INSERT INTO Services  (Service_Code, Description, Cost, Price, Months, Mileage)
+VALUES ('OILCHG', 'Oil Change', 9.95, 10.95, 6, 6000);
+INSERT INTO Services  (Service_Code, Description, Cost, Price, Months, Mileage)
+VALUES ('TIREROTATE', 'Tire Rotation', 6.95, 9.95, 12, 12000);
+INSERT INTO Services  (Service_Code, Description, Cost, Price, Months, Mileage)
+VALUES ('FLUIDS', 'Fluid Replacement', 29.95, 49.96, 30, 30000);
+INSERT INTO Services  (Service_Code, Description, Cost, Price, Months, Mileage)
+VALUES ('TUNEUPBASIC', 'Basic Engine tune up', 69.95, 149.95, 18, 18000);
+  
+CREATE TABLE Parts  (
+  Part_Code       VARCHAR2(20)    PRIMARY KEY,
+  Description     VARCHAR2(30)    NOT NULL,
+  Cost            NUMBER(6,2)     NOT NULL,
+  Price           NUMBER(6,2)     NOT NULL,
+  CHECK(Cost > 0),
+  CHECK(Price > 0),);
+  
