acd2b7bc6904e41d26b064672589e0c10cd200b5
N8theGr811 <104658019+N8theGr811@users.noreply.github.com>
2023-05-30 15:37:49 -0700

    Update task5.sql

--- task5.sql
+++ task5.sql
@@ -1,4 +1,8 @@
 CREATE TABLE Sales_Invoice(
+  invoice_id    NUMBER(6)   PRIMARY KEY,
+  customer_id   NUMBER(6)   NOT NULL REFERENCES customer(customer_id),
+  VIN           NUMBER(6)   NOT NULL REFERENCES sales_vehicle(VIN),
+  employee_id   NUMBER(6)   NOT NULL REFERENCES employee(employee_id),
   
   );
 
