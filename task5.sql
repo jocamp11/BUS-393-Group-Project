9b71c27f4f2aeeaaa7e57027c8e59f6ceeee1582
N8theGr811 <104658019+N8theGr811@users.noreply.github.com>
2023-05-30 15:47:15 -0700

    Update task5.sql

--- task5.sql
+++ task5.sql
@@ -5,6 +5,17 @@ CREATE TABLE Sales_Invoice(
   employee_id   NUMBER(6)   NOT NULL REFERENCES employee(employee_id),
   sale_date     DATE);
 
+CREATE TABLE Vendor (
+  vendor_id     NUMBER(6)   PRIMARY KEY,
+  vendor_name   VARCHAR2(30)  UNIQUE  NOT NULL,
+  contact_name  VARCHAR2(30),
+  street        VARCHAR2(30)  NOT NULL,
+  city          VARCHAR2(30)  NOT NULL,
+  state         VARCHAR2(30)  NOT NULL,
+  zip           VARCHAR2(30)  NOT NULL,
+  phone         VARCHAR2(11)  UNIQUE  NOT NULL,
+  fax           NUMBER(10));
+  
 CREATE TABLE Purchase_Order (
   
   );
@@ -12,3 +23,7 @@ CREATE TABLE Purchase_Order (
   CREATE TABLE Service_Invoice (
     
     );
+
+
+
+
