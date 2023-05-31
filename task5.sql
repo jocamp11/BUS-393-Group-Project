7e24f9fba247e67c602f9f92a563b34276fec6ac
N8theGr811 <104658019+N8theGr811@users.noreply.github.com>
2023-05-30 19:37:56 -0700

    Update task5.sql

--- task5.sql
+++ task5.sql
@@ -42,36 +42,49 @@ CREATE TABLE Purchase_Order (
     
     );
   
-  --Nate's 3 Car Purchases (Step 3 in task 5)
-  -- Create a vendor to buy vehicles from  *this wasn't listed but I beleive we have to do it
-  INSERT INTO Vendor (vendor_id, vendor_name, contact_name, street, city, state, zip, phone, fax)
-  VALUES (100, 'Vintage Auto America', 'Jason Jackson', '1045 Johnson St.', 'Atascadero', 'CA', '93420', 2547368594);
+--Nate's 3 Car Purchases (Step 3 in task 5)
+-- Create a vendor to buy vehicles from  *this wasn't listed but I beleive we have to do it
+INSERT INTO Vendor (vendor_id, vendor_name, contact_name, street, city, state, zip, phone, fax)
+VALUES (100, 'Vintage Auto America', 'Jason Jackson', '1045 Johnson St.', 'Atascadero', 'CA', '93420', 2547368594);
 
-  -- First sales vehicle insert statement
-  INSERT INTO Sales_Vehicle (VIN, year, make, model, exterior_color, trim, mileage, condition, status, purchase_price, list_price)
-  VALUES (17392043928394073, 1963, 'Lincoln', 'Continental', 'black', 'convertible', 'Good', 'FORSALE', 55000, 90000);
+-- First sales vehicle insert statement
+INSERT INTO Sales_Vehicle (VIN, year, make, model, exterior_color, trim, mileage, condition, status, purchase_price, list_price)
+VALUES (17392043928394073, 1963, 'Lincoln', 'Continental', 'black', 'convertible', 'Good', 'FORSALE', 55000, 90000);
   
-  -- PO for Lincoln Continental
-  INSERT INTO Purchase_Order (purchase_ID, terms, VIN, vendor_id, employee_id, purchase_date)
-  VALUES (1, 'cash', 17392043928394073, 100, 1000, '05/29/2023');
+-- PO for Lincoln Continental
+INSERT INTO Purchase_Order (purchase_ID, terms, VIN, vendor_id, employee_id, purchase_date)
+VALUES (1, 'cash', 17392043928394073, 100, 1000, '05/29/2023');
   
-  -- Update of Lincoln Continental purchase order to add manager approval
-  UPDATE Purchase_Order
-  SET approving_manager = 1000
-  WHERE purchase_ID = 1;
+-- Update of Lincoln Continental purchase order to add manager approval
+UPDATE Purchase_Order
+SET approving_manager = 1000
+WHERE purchase_ID = 1;
   
-  -- Second sales vehicle insert statement
-  INSERT INTO Sales_Vehicle (VIN, year, make, model, exterior_color, trim, mileage, condition, status, purchase_price, list_price)
-  VALUES (84638459374937489, 1967, 'Cheverlet', 'Corvette Stingray', 'blue', 'convertible', 'Great', 'FORSALE', 40000, 60000);
+-- Second sales vehicle insert statement
+INSERT INTO Sales_Vehicle (VIN, year, make, model, exterior_color, trim, mileage, condition, status, purchase_price, list_price)
+VALUES (84638459374937489, 1967, 'Cheverlet', 'Corvette Stingray', 'blue', 'convertible', 'Great', 'FORSALE', 40000, 60000);
   
-  -- PO for Cheverlet Stingray
-  INSERT INTO Purchase_Order (purchase_ID, terms, VIN, vendor_id, employee_id, purchase_date)
-  VALUES (2, 'credit', 84638459374937489, 100, 1004, '05/23/2023');
+-- PO for Cheverlet Stingray
+INSERT INTO Purchase_Order (purchase_ID, terms, VIN, vendor_id, employee_id, purchase_date)
+VALUES (2, 'credit', 84638459374937489, 100, 1004, '05/23/2023');
   
-  -- Update of Lincoln Continental purchase order to add manager approval
-  UPDATE Purchase_Order
-  SET approving_manager = 1003
-  WHERE purchase_ID = 2;
+-- Update of Lincoln Continental purchase order to add manager approval
+UPDATE Purchase_Order
+SET approving_manager = 1003
+WHERE purchase_ID = 2;
+  
+-- Third sales vehicle insert statement
+INSERT INTO Sales_Vehicle (VIN, year, make, model, exterior_color, trim, mileage, condition, status, purchase_price, list_price)
+VALUES (93759382749506876, 1962, 'Ford', 'Galaxie 500', 'white', NULL, 'Good', 'FORSALE', 35000, 80000);
+  
+-- PO for Ford Galaxie 500
+INSERT INTO Purchase_Order (purchase_ID, terms, VIN, vendor_id, employee_id, purchase_date)
+VALUES (3, 'cash', 93759382749506876, 100, 1005, '05/26/2023');
+  
+-- Update of Lincoln Continental purchase order to add manager approval
+UPDATE Purchase_Order
+SET approving_manager = 1000
+WHERE purchase_ID = 3;
                         
 
 
