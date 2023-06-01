db7cff57eb0c27ff6bae9a49b60efc1aa5216a80
jocamp11 <124825877+jocamp11@users.noreply.github.com>
2023-06-01 15:04:14 -0700

    Update masterScript.sql

--- masterScript.sql
+++ masterScript.sql
@@ -8,6 +8,8 @@ DROP TABLE Parts CASCADE CONSTRAINTS PURGE;
 DROP TABLE Services CASCADE CONSTRAINTS PURGE;
 DROP TABLE Service_Parts CASCADE CONSTRAINTS PURGE;
 DROP TABLE Services_Provided CASCADE CONSTRAINTS PURGE;
+DROP TABLE Sales_Vehicle CASCADE CONSTRAINTS PURGE
+DROP TABLE Service_Vehicle CASCADE CONSTRAINTS PURGE
 
 --  1 Creating Tables
  
@@ -252,3 +254,33 @@ CREATE OR REPLACE VIEW Part_List
 AS SELECT Part_Code, Description, Cost, Price
 FROM Parts
 ORDER BY Part_Code;
+
+-- Create Sales_Vehicle & Service_Vehicle
+
+CREATE TABLE Sales_Vehicle (
+  VIN             VARCHAR2(17)  PRIMARY KEY,
+  year            NUMBER(4)     NOT NULL,
+  make            VARCHAR2(30)  NOT NULL,
+  model           VARCHAR2(30)  NOT NULL,
+  exterior_color  VARCHAR2(20)  NOT NULL,
+  trim            VARCHAR2(20),
+  mileage         NUMBER(8, 2)  NOT NULL,
+  condition       VARCHAR2(20),
+  status          VARCHAR2(7)  NOT NULL,
+  purchase_price  NUMBER(8, 2),
+  list_price      NUMBER(8, 2),
+  CHECK(status IN ('SOLD', 'FORSALE', 'TRADEIN'),
+  CHECK(mileage > 0),
+  CHECK (purchase_price > 0),
+  CHECK (list_price > 0));
+ -- We are assuming that there are cases where price could be lower than cost 
+ -- (EX: A car isn't selling and Larry is willing to give a good deal to get rid of it)
+  
+
+CREATE TABLE Service_Vehicle (
+  VIN       VARCHAR2(17)  PRIMARY KEY,
+  year      NUMBER(4)     NOT NULL,
+  make      VARCHAR2(30)  NOT NULL,
+  model     VARCHAR2(30)  NOT NULL,
+  mileage   NUMBER(8, 2)  NOT NULL,
+  CHECK(mileage > 0));
