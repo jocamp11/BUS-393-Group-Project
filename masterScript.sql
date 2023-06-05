ba135f14b3301d02ce6a72eeb8c7316857b82dc9
dcow13 <133283939+dcow13@users.noreply.github.com>
2023-06-05 16:21:50 -0700

    Update masterScript.sql

--- masterScript.sql
+++ masterScript.sql
@@ -705,7 +705,7 @@ WHERE status = 'SOLD';
 
 -- Task 4 Query d (Vehicle Inventory Value)
 CREATE OR REPLACE VIEW Inventory_Value
-SELECT SUM(list_price) AS "Total Value ($)"
+AS SELECT SUM(list_price) AS "Total Value ($)"
 FROM sales_vehicle
 WHERE status = 'FORSALE';
 
